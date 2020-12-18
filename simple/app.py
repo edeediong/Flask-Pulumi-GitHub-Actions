e77a647d6fff16eecd7a4296ac4b09647e3e785e
edeediong <edidiongmichael08@gmail.com>
2020-12-18 09:36:37 +0100

    feat: Chose a simpler application

---
+++ simple/app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask import Response
+from flask import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World! (from a Docker container)"
+
+@app.route('/cities.json')
+def cities():
+    data = ["Amsterdam", "San Francisco", "Berlin", "New York"]
+    resp = Response(json.dumps(data), status=200, mimetype="application/json")
+    return resp
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
---
+++ simple/test_app.py
@@ -0,0 +1,20 @@
+from app import app
+
+import unittest
+import json
+
+class CitiesTestCase(unittest.TestCase):
+
+    def test_cities(self):
+        tester = app.test_client(self)
+        response = tester.get('/cities.json', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode("utf-8"), json.dumps(['Amsterdam', 'San Francisco', 'Berlin', 'New York']))
+
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        self.assertEqual(response.data.decode("utf-8"), "Hello, World! (from a Docker container)")
+
+if __name__ == '__main__':
+    unittest.main()
