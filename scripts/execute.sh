42d239544f34149551c823e4b15349b836c93dd7
edeediong <edidiongmichael08@gmail.com>
2020-12-29 01:52:34 +0100

    final destruction of resources
    Signed-off-by: edeediong <edidiongmichael08@gmail.com>

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-cd ~/accure
+cd /home/ubuntu/accure
 
 python3.6 -m venv accurevenv
 source accurevenv/bin/activate
 pip install -r requirements.txt
 
-sudo cp -rf /home/ubuntu/accure/gunicorn.service /etc/systemd/system/
+sudo cp -rf /home/ubuntu/accure/accure.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 
-sudo systemctl start gunicorn
+sudo systemctl start accure
 
 echo "Gunicorn has started."
 
-sudo systemctl enable gunicorn
+sudo systemctl enable accure
 
 echo "Gunicorn has been enabled."
 
-sudo systemctl status gunicorn
+sudo systemctl status accure
 
-sudo systemctl restart gunicorn
+sudo systemctl restart accure
